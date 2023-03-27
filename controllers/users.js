c041a13be0285b63ae5942d943067fe49ab3234a
amalraj-g <g.amalraj1993@gmail.com>
2023-03-27 22:28:05 +0530

    content added

--- controllers/users.js
+++ controllers/users.js
@@ -3,22 +3,27 @@ import jwt from 'jsonwebtoken';
 
 import User from '../models/users.js';
 
+const ok=200;
+const Not_found = 404;
+const badRequest = 400;
+const serverError =500;
+
 export const signin = async (req, res) => {
     const { email, password } = req.body;
     try{
         console.log("signin");
 
         const existingUser = await User.findOne({ email});
-        if(!existingUser) return res.status(404).json({ message: "User doesnot exist"}) ;
+        if(!existingUser) return res.status(Not_found).json({ message: "User doesnot exist"}) ;
         
         const IsPasswordCorrect = await bcrypt.compare(password, existingUser.password);
-        if(!IsPasswordCorrect)return res.status(400).json({ message: "Invalid credential"});
+        if(!IsPasswordCorrect)return res.status(badRequest).json({ message: "Invalid credential"});
             
         const token = jwt.sign({email:existingUser.email, id: existingUser._id} ,'winner', {expiresIn:"1hr"});
-        res.status(200).json({ token});
+        res.status(ok).json({ token});
 
     } catch(error){
-        res.status(500).json({message:'somethng went wrong'});
+        res.status(serverError).json({message:'somethng went wrong'});
 
     }
 
